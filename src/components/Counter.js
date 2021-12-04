d344dd5343645eea0adf716b1d9031c868c6be55
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-12-04 10:53:09 +0100

    multiple state properties

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,6 +6,7 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({type: 'increment'});
@@ -16,16 +17,17 @@ const Counter = () => {
   };
 
   const increaseHandler = () => {
-    dispatch({type:'increase', amount: 5})
-  }
+    dispatch({type: 'increase', amount: 5});
+  };
 
   const toggleCounterHandler = () => {
+    dispatch({type: 'toggle'});
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increase by 5</button>
